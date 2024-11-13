1aca0e38480f16e4486a755dfb26611e6f23122d
chr1swill <alexanderwilliams20003@gmail.com>
2024-11-12 21:03:46 -0500

    feat: format and move logic to a macro

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <assert.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define OUT STDOUT_FILENO
 
@@ -13,28 +13,28 @@ struct string_t {
 };
 
 struct string_t string_from(int n, char *data) {
-  struct string_t res = { .len = n, .data = data };
+  struct string_t res = {.len = n, .data = data};
   return res;
 }
 
 struct string_t file_to_buff(const char *filepath) {
-    assert(filepath);
+  assert(filepath);
 
-    FILE *f = fopen(filepath, "rb");
-    assert(f);
+  FILE *f = fopen(filepath, "rb");
+  assert(f);
 
-    fseek(f, 0, SEEK_END);
-    long m = ftell(f);
-    assert(m >= 0);
-    fseek(f, 0, SEEK_SET);
-    char *buffer = calloc(1, sizeof(char) * (size_t) m + 1);
-    assert(buffer);
+  fseek(f, 0, SEEK_END);
+  long m = ftell(f);
+  assert(m >= 0);
+  fseek(f, 0, SEEK_SET);
+  char *buffer = calloc(1, sizeof(char) * (size_t)m + 1);
+  assert(buffer);
 
-    size_t n = fread(buffer, 1, (size_t) m, f);
-    assert(n == (size_t) m);
+  size_t n = fread(buffer, 1, (size_t)m, f);
+  assert(n == (size_t)m);
 
-    fclose(f);
-    return string_from(n, buffer);
+  fclose(f);
+  return string_from(n, buffer);
 }
 
 struct string_t chop_until_delim(struct string_t *string, const char delim) {
@@ -43,7 +43,8 @@ struct string_t chop_until_delim(struct string_t *string, const char delim) {
   };
 
   size_t i = 0;
-  while (i < string->len && string->data[i] != delim) i++;
+  while (i < string->len && string->data[i] != delim)
+    i++;
 
   struct string_t line;
   line.data = string->data;
@@ -60,9 +61,7 @@ struct string_t chop_until_delim(struct string_t *string, const char delim) {
   return line;
 }
 
-void compile_c_code(struct string_t s) {
-  printf("%.*s\n", (int) s.len, s.data);
-}
+void compile_c_code(struct string_t s) { printf("%.*s\n", (int)s.len, s.data); }
 
 void compile_byte_array(struct string_t s) {
   printf("write(OUT, \"");
@@ -72,24 +71,40 @@ void compile_byte_array(struct string_t s) {
   printf("\", %lu);\n", s.len);
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("Ussage: %s <template_filepath.>\n", argv[0]);
-    return -1;
-  }
+#define COMPILE_TEMPLATE(template_filepath)                                   \
+  do {                                                                         \
+    struct string_t template = file_to_buff((template_filepath));              \
+    int c_code_mode = 0;                                                       \
+    while (template.len) {                                                     \
+      struct string_t token = chop_until_delim(&template, '%');                \
+      if (c_code_mode) {                                                       \
+        compile_c_code(token);                                                 \
+      } else {                                                                 \
+        compile_byte_array(token);                                             \
+      }                                                                        \
+      c_code_mode = !c_code_mode;                                              \
+    }                                                                          \
+  } while (0);
 
-  struct string_t template = file_to_buff(argv[1]);
+int main(/**int argc, char **argv*/ void) {
+  // if (argc != 2) {
+  //   printf("Ussage: %s <template_filepath.>\n", argv[0]);
+  //   return -1;
+  // }
 
-  int c_code_mode = 0;
-  while (template.len) {
-    struct string_t token = chop_until_delim(&template, '%');
-    if (c_code_mode) {
-      compile_c_code(token);
-    } else {
-      compile_byte_array(token);
-    }
-    c_code_mode = !c_code_mode;
-  }
+  // struct string_t template = file_to_buff(argv[1]);
 
+  // int c_code_mode = 0;
+  // while (template.len) {
+  //   struct string_t token = chop_until_delim(&template, '%');
+  //   if (c_code_mode) {
+  //     compile_c_code(token);
+  //   } else {
+  //     compile_byte_array(token);
+  //   }
+  //   c_code_mode = !c_code_mode;
+  // }
+
+  COMPILE_TEMPLATE("test.html");
   return 0;
 }
